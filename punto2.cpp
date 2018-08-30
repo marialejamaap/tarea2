f15103c213355c72f60edc137bc913bb3c820f03
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-08-30 13:03:25 -0500

    Create punto2.cpp

---
+++ punto2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int N, sum_even=0 , sum_odd=0, i=0 ;
+	cout<<" Enter number: " ; 
+cin>>N;
+for (i=0;i<=N;i++) {
+	if (i%2==0){
+		sum_even=sum_even+i;
+	}
+	else{
+		sum_odd=sum_odd+i;
+	}
+}
+cout<<"\n sum of even number is: " << sum_even;
+	cout <<"\n sum of odd number is: " << sum_odd;
+	return 0;
+}
