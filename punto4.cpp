1961a7aec5066057147d4e8563712ef6eabb9f12
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-09-05 16:55:58 -0500

    Update punto4.cpp

--- punto4.cpp
+++ punto4.cpp
@@ -1,23 +1,42 @@
+/* Escriba una aplicación que imprima en la consola una figura triangular basada en un caracter
+y de un número de filas ingresado por el usuario. Por ejemplo, ingresando el caracter “*” y
+tamaño 5 la pirámide tiene la apariencia:
+         *
+        **
+       ***
+      ****
+     *****
+    ******
+   *******
+  ********
+ ********* */
+
 
 #include <iostream>
 #include <string>
 using namespace std;
 
-int triangulo(int a);
-int main(){
-    cout<<"Triangulo de Caracteres"<<endl <<endl; 
- int c;
- cout<<"ingrese un valor para filas y columnas...1,2,3...: ";
- cin>>c;
- cout<<triangulo(c);
- }
+int main()
+{
+ int i,j,n,aux;
+ string sp;
+ string sr;
+ cout<<"ingrese un numero para filas y columnas del triangulo: ";
+ cin>>n;
+ cout<<" \n ingrese el caracter que quiere utilizar: ";
+ cin>>sp;
+ cout<<endl;
+ aux=n;
+ sr=sp;
  
- int triangulo (int a){
-     int i,j;
- for(i=1;i<=a;i++){
-     for(j=1;i>=j;j++)
-     cout<<"*";
-     cout<<"\n"; 
+ for (j=1;j<=aux;j++){
+ for (i=1;i<=n;i++){
+     cout<<" ";
      }
-     return a;}
-     
+     cout<<sp;
+cout<<endl;
+   n=n-1;
+sp=sp+sr;
+}
+
+}
