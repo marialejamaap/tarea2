12cba1ee345a06d6e9b225aa0fad0ad517dfceed
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-09-05 17:24:16 -0500

    Update punto5.cpp

--- punto5.cpp
+++ punto5.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include<string>
 
+
+using namespace std;
+
+int main(){
+string phrase;
+int count_a=0, count_e=0,count_i=0, count_o=0,count_u=0,count_vocales=0,count_consonante=0;
+cout<<"introduzca una frase: ";
+getline(cin, phrase);
+for (char c: phrase){
+
+if ((c=='a')|| (c=='A'))
+count_a=count_a+1;
+else if ((c=='e')|| (c=='E'))
+
+count_e=count_e+1;
+else if ((c=='i')|| (c=='I')) 
+
+count_i=count_i+1;
+else if ((c=='o')|| (c=='O'))
+count_o=count_o+1;
+else if ((c=='u')||(c=='U'))
+
+count_u=count_u+1;
+
+else 
+
+count_consonante++;}
+count_vocales=count_a+count_e+count_i+count_o+count_u;
+cout<<"total de vocales son: " << count_vocales;
+cout <<"\n total de consonantes son: "<<count_consonante;
+}
