8a2af288c5dddcba02a5ba25cd888cb3fd3ede7d
Maria Acevedo <42081532+marialejamaap@users.noreply.github.com>
2018-09-05 21:46:24 -0500

    Update punto5.cpp

--- punto5.cpp
+++ punto5.cpp
@@ -8,13 +8,18 @@ número total de vocales y el número total de consonantes que se encuentran en 
 
 
 using namespace std;
-
+string palabra(string str);
 int main(){
 string phrase;
-int count_a=0, count_e=0,count_i=0, count_o=0,count_u=0,count_vocales=0,count_consonante=0;
 cout<<"introduzca una frase: ";
 getline(cin, phrase);
-for (char c: phrase){
+palabra(phrase);
+}
+
+string palabra(string str){
+
+int count_a=0, count_e=0,count_i=0, count_o=0,count_u=0, count_vocales, count_consonante;
+for (char c: str){
 
 if ((c=='a')|| (c=='A'))
 count_a=count_a+1;
@@ -33,7 +38,8 @@ count_u=count_u+1;
 else 
 
 count_consonante++;}
+
 count_vocales=count_a+count_e+count_i+count_o+count_u;
 cout<<"total de vocales son: " << count_vocales;
-cout <<"\n total de consonantes son: "<<count_consonante;
+cout<<"\n total de consonantes son: "<<count_consonante;
 }
